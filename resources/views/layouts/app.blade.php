155b575597fb6728fc6e9715cd7ffe7f32420e49
Soloski123 <ahambasolomon800@gmail.com>
2020-03-28 22:54:46 -0700

    securing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -81,6 +81,6 @@
             'user' => Auth::user()
         ]) !!}
     </script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
 </body>
 </html>
